ef92451347c5e7934f4afdacd589a111ff7b33d2
thoenis <sthoeni@kof.ethz.ch>
2017-10-26 09:00:28 +0200

    ts are now stripped of leading/trailing NA

--- R/json_to_ts.R
+++ R/json_to_ts.R
@@ -1,7 +1,7 @@
 #' Helper to turn api-returned json (lists) into lists of ts objects
 #' @importFrom xts xts
 json_to_ts <- function(json_data) {
-  xt <- xts(json_data$value, order.by = zoo::as.yearmon(json_data$date))
+  xt <- na.trim(xts(json_data$value, order.by = zoo::as.yearmon(json_data$date)))
   if(frequency(xt) < Inf) {
     as.ts(xt, start = start(xt), end = end(xt))
   } else {
