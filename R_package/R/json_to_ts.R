16f05d5617271f5b75994b5897adf886c85837f4
thoenis <sthoeni@kof.ethz.ch>
2017-10-30 15:06:55 +0100

    Added import for zoo in json_to_ts

--- R_package/R/json_to_ts.R
+++ R_package/R/json_to_ts.R
@@ -1,7 +1,8 @@
 #' Helper to turn api-returned json (lists) into lists of ts objects
 #' @importFrom xts xts
+#' @import zoo
 json_to_ts <- function(json_data) {
-  xt <- na.trim(xts(json_data$value, order.by = zoo::as.yearmon(json_data$date)))
+  xt <- na.trim(xts(json_data$value, order.by = as.yearmon(json_data$date)))
   if(frequency(xt) < Inf) {
     as.ts(xt, start = start(xt), end = end(xt))
   } else {
