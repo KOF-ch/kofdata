00e19a97cd54c59280d9aaec7bcc499c82f94857
mbannert <matthias.bannert@gmail.com>
2017-11-01 16:21:45 +0100

    split R and python version into two repos

--- python_package/kofdata/errors.py
+++
@@ -1,2 +0,0 @@
-class KofdataError(Exception):
-	pass	# Will have to do for now. Some meaningful members would be neat.
--- python_package/kofdata/list_cached_files.py
+++
@@ -1,4 +0,0 @@
-from helpers import get_cdc_files
-
-def list_cached_files(username, api_key):
-	return get_cdc_files(username, api_key)
