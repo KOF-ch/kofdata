1e82e6f381259ba33fdb3bca779d38528c925051
thoenis <sthoeni@kof.ethz.ch>
2017-11-16 12:58:37 +0100

    Added translate_legacy_key

---
+++ R/translate_legacy_keys.R
@@ -0,0 +1,22 @@
+#' translate_legacy_keys
+#' 
+#' Gives ts_keys for the given legacy keys.
+#' Since there is no one to one mapping from old to new keys, it is possible that multiple new keys are returned for an old key.
+#' Note: Not all possible new keys are returned. The keys returned by this function should serve as a starting point to find the
+#' specific new keys that are needed. For the key explorer to find the exact keys, see \code{\link{start_key_explorer}}.
+#' @param legacy_keys A vector of legacy keys for which to find new keys
+#' @return A list of vectors of new ts_keys
+#' @import httr
+#' @import jsonlite
+#' @export
+translate_legacy_keys <- function(legacy_keys) {
+  
+  legacy_keys <- paste(legacy_keys, collapse=",")
+  
+  url_template <- "https://datenservice.kof.ethz.ch/api/v1/metadata/translatelegacykeys?keys=%s"
+  
+  url <- sprintf(url_template, legacy_keys)
+  
+  response <- GET(url)
+  fromJSON(content(response, as="text"))
+}
