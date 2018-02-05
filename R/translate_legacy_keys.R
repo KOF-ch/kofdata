0f6b5bef3cb47efa2576db965087bace2aa0fa2a
mbannert <matthias.bannert@gmail.com>
2018-02-05 16:58:53 +0100

    removed function translate_legacy_key cause it was a duplicate, introduced chunking for translatekeys

--- R/translate_legacy_keys.R
+++ R/translate_legacy_keys.R
@@ -5,16 +5,33 @@
 #' Note: Not all possible new keys are returned. The keys returned by this function should serve as a starting point to find the
 #' specific new keys that are needed. For the key explorer to find the exact keys, see \code{\link{start_key_explorer}}.
 #' @param legacy_keys A vector of legacy keys for which to find new keys
+#' @param chunksize integer to set chunksize in order to avoid URLs that are too long. 
 #' @return A list of vectors of new ts_keys
 #' @import httr
 #' @import jsonlite
 #' @export
-translate_legacy_keys <- function(legacy_keys) {
+translate_legacy_keys <- function(legacy_keys, chunksize = 100){
   
   url <- "https://datenservice.kof.ethz.ch/api/v1/metadata/translatelegacykeys"
   
-  legacy_keys <- paste(legacy_keys, collapse=",")
   
-  response <- GET(url, query = list(keys = legacy_keys))
-  fromJSON(content(response, as="text"))
+  chunk_list <- split(legacy_keys,ceiling(seq_along(legacy_keys)/chunksize))
+  
+  result <- lapply(chunk_list,function(x){
+    l_keys <- paste(x, collapse=",")
+    response <- GET(url, query = list(keys = l_keys))
+    if(response$status_code != 200){
+      stop(sprintf("web request not successful, http error %s returned",
+                   as.character(response$status_code)))
+    }
+    result <- fromJSON(content(response, as="text"))
+  })
+  
+  
+  out <- unlist(result,recursive = F)
+  names(out) <- gsub("[0-9]+\\.","",names(out))
+  out
 }
+
+
+
