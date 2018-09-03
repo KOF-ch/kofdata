0e6e93d1ba5492f216944bc303c780f4d527f354
thoenis <sthoeni@kof.ethz.ch>
2018-09-03 08:58:46 +0200

    Replace non-working key in tests

--- tests/testthat/test_get_metadata.R
+++ tests/testthat/test_get_metadata.R
@@ -2,7 +2,7 @@ context("get_metadata")
 
 test_that("get_metadata works", {
   expect_true(is.na(get_metadata("nokey")))
-  expect_false(is.na(get_metadata("ch.kof.bau.run1.ng08.f12.q_cb_restrict_none.ans_count.d11")))
+  expect_false(is.na(get_metadata("ch.kof.inu.ng08.fx.sector_kof.cm.q_ql_chg_price_sales_p3m.share_eq")))
   expect_error(get_metadata(locale="fi"))
   
   some_meta <- get_metadata(c("ch.kof.bau.run1.ng08.f12.q_cb_restrict_none.ans_count.d11",
