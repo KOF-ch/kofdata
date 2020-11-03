0bd15731007eae712573612b18af978040ead4fb
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-11-03 16:58:19 +0100

    fix up list_keys_in_set

--- R/list_available_sets.R
+++ R/list_available_sets.R
@@ -1,7 +1,7 @@
 #' List Available Set Names and Their Description
 #'
 #' Sets are pre-defined lists of time series. Sets are the convenient alternative to concatenating many series in an URL.
-#' @param api_key Your API key. This is only needed if accessing non-public time sets
+#' @param api_key Your API key. This is only needed if accessing non-public sets
 #' @return A data frame with the names, descriptions and public availability of datasets. These sets can be downloaded via get_dataset.
 #' @import httr
 #' @import jsonlite
