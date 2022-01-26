fb016c103dad03e39d6db7091d56a3d3242f7b90
Diana Diaz <didiaz@rstudio2.kof.ethz.ch>
2022-01-26 15:04:54 +0100

    re-add list_public_keys with deprication warning

---
+++ R/list_public_keys.R
@@ -0,0 +1,10 @@
+#' List Keys for All Publicly Available Time Series
+#'
+#'This function will be removed from future versions of kofdata.
+#'
+#' @return An array of all public time series keys.
+#' @examples 
+#' list_public_keys()
+#' @export
+list_public_keys <- function() {.Defunct(msg = "This funtion has been removed.")}
+
