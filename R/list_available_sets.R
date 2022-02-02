866bf39ac05b975ae9f78a2ddacd1f9cfe6504e3
Diana Diaz <didiaz@rstudio2.kof.ethz.ch>
2022-02-02 11:22:00 +0100

    list_available_sets deprecated in favour of list_available_collections.

--- R/list_available_sets.R
+++ R/list_available_sets.R
@@ -1,5 +1,6 @@
 #' List Available Set Names and Their Description
 #'
+#' This function is deprecated. Use \code{\link{list_available_collections}} instead.
 #' Sets are pre-defined lists of time series. Sets are the convenient alternative to concatenating many series in an URL.
 #' @param api_key Your API key. This is only needed if accessing non-public sets
 #' @return A data frame with the names, descriptions and public availability of datasets. These sets can be downloaded via get_dataset.
