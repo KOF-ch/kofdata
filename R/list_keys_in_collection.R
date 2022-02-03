0d02df41675177790645eff896779edfb0a89e50
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2022-02-03 10:28:56 +0100

    pass apikey in requests to list keys in non-public collection

--- R/list_keys_in_collection.R
+++ R/list_keys_in_collection.R
@@ -12,10 +12,11 @@
 #' @export
 list_keys_in_collection <- function(collectionname, api_key = NULL) {
   url <- "https://datenservice.kof.ethz.ch/api/v1/%s/collections/details/%s"
-  
+
   keylists <- lapply(collectionname, function(collection){
-    response <- GET(sprintf(url, ifelse(is.null(api_key), "public", "main"), collection))
-    
+    response <- GET(sprintf(url, ifelse(is.null(api_key), "public", "main"), collection),
+                    query = list(apikey = api_key))
+
     if(response$status_code == 200) {
       fromJSON(content(response, as = "text"))$keys
     } else if(response$status_code == 404){
@@ -24,9 +25,9 @@ list_keys_in_collection <- function(collectionname, api_key = NULL) {
       stop(sprintf("An error occurred when calling the api:\nStatus: %d\nContent:%s", response$status_code, content(response, as = "text")))
     }
   })
-  
+
   names(keylists) <- collectionname
-  
+
   if(length(collectionname) > 1) {
     keylists
   } else {
